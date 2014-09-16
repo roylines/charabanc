bbf42dae3300eecab1e5b8944821f71cb08a6108
Roy Lines <roy@roylines.co.uk>
2014-09-16 18:37:29 +0100

    refactored memory transport

--- index.js
+++ index.js
@@ -1,37 +1,9 @@
-var _ = require('lodash'),
-    async = require('async');
+var transport = require('./lib/transports/memory');
 
 var charabanc = {};
 
-var registry = {
-};
-
-charabanc.register = function(action, service, done) {
-  if(registry[action] === undefined) {
-    registry[action] = [service];
-  } else {
-    registry[action].push(service);
-  }
-
-  return done();
-};
-
-charabanc.unregister = function(action, service, done) {
-  _.remove(registry[action], function(s) {
-    return s === service; 
-  });
-
-  return done();
-};
-
-charabanc.send = function(action, payload, done) {
-  var actions = _.map(registry[action], function(service) {
-    return function(cb) {
-      return service(payload, cb);
-    };
-  });
-
-  return async.parallel(actions, done);
-};
+charabanc.register = transport.register;
+charabanc.unregister = transport.unregister; 
+charabanc.send = transport.send;
 
 module.exports = charabanc;
