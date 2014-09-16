a8ab5d8944651ce3451b925244579717cabd7256
Roy Lines <roy@roylines.co.uk>
2014-09-16 18:23:42 +0100

    added simple routing for 1 and 2 services

--- index.js
+++ index.js
@@ -1,15 +1,37 @@
+var _ = require('lodash'),
+    async = require('async');
+
 var charabanc = {};
 
+var registry = {
+};
+
 charabanc.register = function(action, service, done) {
+  if(registry[action] === undefined) {
+    registry[action] = [service];
+  } else {
+    registry[action].push(service);
+  }
+
   return done();
 };
 
 charabanc.unregister = function(action, service, done) {
+  _.remove(registry[action], function(s) {
+    return s === service; 
+  });
+
   return done();
 };
 
-charabanc.send = function(action, data, done) {
-  return done();
+charabanc.send = function(action, payload, done) {
+  var actions = _.map(registry[action], function(service) {
+    return function(cb) {
+      return service(payload, cb);
+    };
+  });
+
+  return async.parallel(actions, done);
 };
 
 module.exports = charabanc;
--- test/charabanc.js
+++ test/charabanc.js
@@ -1,19 +1,47 @@
 var charabanc = require('../index.js'),
+    chai = require('chai'),
   sinon = require('sinon');
+  
+chai.should();
 
 describe('charabanc', function() {
   describe('with one service', function() {
-    var service = sinon.stub();
+    var service;
     before(function(done) {
+      service = sinon.stub();
+      service.yields();
       charabanc.register('act', service, done);
     });
     after(function(done) {
       charabanc.unregister('act', service, done);
     });
-    it('should', function(done) {
-      return done();
+    it('should call service on send', function(done) {
+      charabanc.send('act', { data: 'DATA'}, function(e) {
+        service.calledOnce.should.be.true;
+        return done(e);
+      });      
     });
   });
-
-
+  describe('with two services', function() {
+    var service1, service2;
+    before(function(done) {
+      service1 = sinon.stub();
+      service1.yields();
+      service2 = sinon.stub();
+      service2.yields();
+      charabanc.register('act', service1, done);
+      charabanc.register('act', service2, done);
+    });
+    after(function(done) {
+      charabanc.unregister('act', service1, done);
+      charabanc.unregister('act', service2, done);
+    });
+    it('should call each service on send', function(done) {
+      charabanc.send('act', { data: 'DATA'}, function(e) {
+        service1.calledOnce.should.be.true;
+        service2.calledOnce.should.be.true;
+        return done(e);
+      });      
+    });
+  });
 });
