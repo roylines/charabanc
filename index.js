f4d7621ef15adb3fd2603f0a44cbd6e885f90d03
Roy Lines <roy@roylines.co.uk>
2014-12-17 19:42:10 +0000

    allow changing of transport

--- index.js
+++ index.js
@@ -1,12 +1,14 @@
-var transport = require('./lib/transport/memory');
-
 var charabanc = {};
 
-charabanc.configure = transport.configure;
-charabanc.register = transport.register;
-charabanc.unregister = transport.unregister; 
-charabanc.request = transport.request;
-charabanc.reset = transport.reset;
-charabanc.dump = transport.dump;
+charabanc.setTransport = function(transport) {
+  charabanc.configure = transport.configure;
+  charabanc.register = transport.register;
+  charabanc.unregister = transport.unregister;
+  charabanc.request = transport.request;
+  charabanc.reset = transport.reset;
+  charabanc.dump = transport.dump;
+};
+
+charabanc.setTransport(require('./lib/transport/memory'));
 
 module.exports = charabanc;
